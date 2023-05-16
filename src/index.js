61479fce55b14e54a3542453fa6a44914d93faf2
bdaneels <brecht.daneels@hotmail.com>
2023-05-16 22:21:05 +0200

    work on dom

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const weatherHandler = (() => {
         }
            weatherObject = processdata
             console.log(weatherObject)
+            domHandler.constructDom(weatherObject)
         }
         catch(error) {
          console.log(error)
@@ -37,5 +38,51 @@ const weatherHandler = (() => {
 
 })()
 
+
+const domHandler = (() => {
+
+    async function constructDom(object){
+        let locationEl = document.getElementsByClassName('card-location')[0]
+        locationEl.textContent = object.location
+
+        let countryEl = document.getElementsByClassName('card-country')[0]
+        countryEl.textContent = object.country
+
+        const today = new Date();
+
+        // Get the date components
+        const year = today.getFullYear();
+        const month = today.getMonth() + 1; // Months are zero-based, so adding 1
+        const day = today.getDate();
+        const hours = today.getHours();
+        const minutes = today.getMinutes();
+
+        // Create a formatted string
+        const formattedDate = `${day}-${month}-${year}`
+        const formattedTime = `${hours}:${minutes}`
+
+        let dateEl = document.getElementsByClassName('card-date')[0]
+        dateEl.textContent = formattedDate
+
+        let timeEl = document.getElementsByClassName('card-time')[0]
+        timeEl.textContent = formattedTime
+
+        let tempEl = document.getElementsByClassName('temp')[0]
+        tempEl.textContent = object.tempC + "°"
+
+        let conditionEl = document.getElementsByClassName('condition')[0]
+        conditionEl.textContent = object.conditionText
+
+        let windEl = document.getElementsByClassName('wind')[0]
+        windEl.textContent = object.windDir
+
+        let conditionImg = document.getElementsByClassName('img')[0]
+        conditionImg.src = "http:" + object.conditionUrl
+    }
+return{
+    constructDom
+}
+
+})()
 
 weatherHandler.setWeather('stockholm');
